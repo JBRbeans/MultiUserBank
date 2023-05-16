b8df9b72d31636673d1e33c030c6c2043e622981
Rusty <107262647+RustyBeans@users.noreply.github.com>
2023-05-15 22:58:55 -0700

    first commit

---
+++ MultiUserBank_Ritola/Bank.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiUserBank_Ritola
+{
+	public class Bank
+	{
+		private const decimal InitialBankBalance = 10000;
+		private decimal bankBalance;
+		private List<Customer> customers;
+		private Customer loggedInUser;
+
+		public decimal BankBalance => bankBalance;
+
+		public Bank()
+		{
+			bankBalance = InitialBankBalance;
+			customers = new List<Customer>();
+
+			// Add customers to the bank
+			customers.Add(new Customer("jlennon", "johnny", 1250));
+			customers.Add(new Customer("pmccartney", "pauly", 2500));
+			customers.Add(new Customer("gharrison", "georgy", 3000));
+			customers.Add(new Customer("rstarr", "ringoy", 1000));
+
+			loggedInUser = null;
+		}
+
+
+		public void MenuInformation()
+		{
+			Console.WriteLine("-Type a number and press the 'Enter' key to select an option-");
+			Console.WriteLine("-0: -Exit the application-");
+			Console.WriteLine("-1: -Display your current balance-");
+			Console.WriteLine("-2: -Withdraw an amount from your balance-");
+			Console.WriteLine("-3: -Deposit an amount into your balance-");
+			Console.WriteLine("-4: -Log out of the current account-");
+			Console.WriteLine("-5: -Log in-");
+
+		}
+		public bool Login(string username, string password)
+		{
+			Customer customer = GetCustomerByUsername(username);
+			if (customer != null && customer.Password == password)
+			{
+				loggedInUser = customer;
+				Console.WriteLine("Logged in as: {0}", loggedInUser.Username);
+				return true;
+			}
+
+			return false;
+		}
+		
+
+		public void Logout()
+		{
+			if (loggedInUser != null)
+			{
+				Console.WriteLine("Logged out.");
+				loggedInUser = null;
+			}
+		}
+		public bool IsSomeoneLoggedIn()
+		{
+			if (loggedInUser == null)
+			{
+				return false;
+			}
+			if (loggedInUser != null)
+			{
+				return true;
+			}
+			return true;
+			
+		}
+		public void DisplayBankBalance()
+		{
+			Console.WriteLine("Bank balance: {0:C}", bankBalance);
+		}
+
+		public void DisplayUserBalance()
+		{
+			if (loggedInUser != null)
+			{
+				decimal balance = loggedInUser.GetBalance();
+				Console.WriteLine("Your balance: {0:C}", balance);
+			}
+		}
+
+		public void Deposit(decimal amount)
+		{
+			if (loggedInUser != null)
+			{
+				loggedInUser.Deposit(amount);
+				Console.WriteLine("Deposit successful. Current balance: {0:C}", loggedInUser.GetBalance());
+			}
+		}
+
+		public decimal Withdraw(decimal amount)
+		{
+			if (loggedInUser != null)
+			{
+				decimal withdrawalAmount = Math.Min(amount, loggedInUser.GetBalance());
+				loggedInUser.Withdraw(withdrawalAmount);
+				Console.WriteLine("Withdrawal successful. Amount withdrawn: {0:C}, Current balance: {1:C}", withdrawalAmount, loggedInUser.GetBalance());
+				return withdrawalAmount;
+			}
+
+			return 0;
+		}
+
+		private Customer GetCustomerByUsername(string username)
+		{
+			return customers.Find(customer => customer.Username == username);
+		}
+
+
+		private class Customer
+		{
+			public string Username { get; }
+			public string Password { get; }
+			private decimal balance;
+
+
+			public Customer(string username, string password, decimal initialBalance)
+			{
+				Username = username;
+				Password = password;
+				balance = initialBalance;
+			}
+
+			public decimal GetBalance()
+			{
+				return balance;
+			}
+
+			public void Deposit(decimal amount)
+			{
+				balance += amount;
+			}
+
+			public void Withdraw(decimal amount)
+			{
+				decimal withdrawalAmount = Math.Min(amount, balance);
+				balance -= withdrawalAmount;
+			}
+		}
+	}
+	
+}
---
+++ MultiUserBank_Ritola/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace MultiUserBank_Ritola
+{
+	internal class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("-Welcome to the Multi-User Bank Co-");
+			Console.WriteLine("-");
+
+			Console.WriteLine("-Please Log in by following these instructions to log in-");
+			Console.WriteLine("-");
+
+			Console.WriteLine("-Enter your username, and then press the 'Enter' key.-");
+			Console.WriteLine("-On the next line, enter your password and then press the 'Enter' key.-");
+			Console.WriteLine("-");
+
+			Bank bank = new Bank();
+			string user;
+			string pass;
+			Login();
+
+
+
+			
+			//bank.Login(Console.ReadLine(), Console.ReadLine());
+			//Console.WriteLine("What would you like to do?");
+			//MenuInformation();
+
+			
+
+			bool isRunning = true;
+			while (isRunning)
+			{
+
+				
+
+				Console.WriteLine("Hello {0:C}! What would you like to do?", user);
+				bank.MenuInformation();
+
+				int option = 0;
+				option = int.Parse(Console.ReadLine());
+
+				switch (option)
+				{
+					// EXIT
+					case 0:
+						isRunning = false;
+						Console.WriteLine("Goodbye, {0:C}!", user);
+						break;
+
+					// DISPLAY BALANCE
+					case 1:
+						if (bank.IsSomeoneLoggedIn() == false)
+						{
+							NotLoggedIn();
+						}
+						Console.WriteLine("Hello {0:C}, welcome back.", user);
+						bank.DisplayUserBalance();
+						break;
+
+					// WITHDRAW
+					case 2:
+						if (bank.IsSomeoneLoggedIn() == false)
+						{
+							NotLoggedIn();
+						}
+						
+						Console.WriteLine("Hello, {0:C}! Please enter how much would you like to withdraw:", user);
+						decimal withdrawalAmt = decimal.Parse(Console.ReadLine());
+						if (withdrawalAmt > 500)
+						{
+							Console.WriteLine("Hello, {0:C}! Please choose an amount less than $500.00", user);
+							break;
+						}
+						bank.Withdraw(withdrawalAmt);
+
+						break;
+
+					// DEPOSIT
+					case 3:
+						if (bank.IsSomeoneLoggedIn() == false)
+						{
+							NotLoggedIn();
+						}
+						Console.WriteLine("Hello, {0:C}! Please enter how much would you like to deposit:", user);
+						decimal depositAmt = decimal.Parse(Console.ReadLine());
+						bank.Deposit(depositAmt);
+						break;
+
+					// LOG OUT USER
+					case 4:
+						if (bank.IsSomeoneLoggedIn() == false)
+						{
+							NotLoggedIn();
+						}
+						bank.Logout();
+						Console.WriteLine("Goodbye, {0:C}!", user);
+						break;
+
+					// LOG IN USER
+					case 5:
+						Login();
+						Console.WriteLine("Hello, {0:C}!", user);
+						break;
+
+					default: break;
+						
+				}
+			}
+			void Login()
+			{
+			
+				Console.WriteLine("Please enter your username:");
+				user = Console.ReadLine();
+				Console.WriteLine("Please enter your password:");
+				pass = Console.ReadLine();
+				bank.Login(user, pass);
+				
+				
+			}
+			void NotLoggedIn()
+			{
+				Console.WriteLine("*WARNING*\n*You are not logged in to your account*\n\n*Please Log in to continue*");
+				Login();
+			}
+			
+		}
+		
+	}
+}
